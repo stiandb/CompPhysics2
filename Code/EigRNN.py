e1f001d89b74e185715d666c1b0b271ff7d4c8ef
Heine Olsson Aabø <heine2307@MacBook-Pro-2.local>
2019-05-31 19:34:56 +0200

    Fix convergence

--- Code/EigRNN.py
+++ Code/EigRNN.py
@@ -1,68 +1,77 @@
 import numpy as np
-from hamiltonian import *
+import time
 import sys
+import matplotlib.pyplot as plt
 
 class EigRNN:
-	"""
-	Finds the lowest eigenvalue of a symmetric matrix
-	by minimizing the Rayleigh quotient with gradient
-	descent
-	"""
-	def __init__(self,A,eps=1e-4,maxiter=1000):
-		"""
-		A - input matrix
-		eps - stops iterating when eigenvalue is not changing more than eps
-		maxiter - maximum number of iterations
-		"""
-		self.A = A
-		self.maxiter = maxiter
-		self.eps = eps
-		self.x = np.random.rand(A.shape[0])
-	
-	def dR(self):
-		"""
-		Returns the gradient of the Rayleigh quotient
-		"""
-		x = self.x
-		self.Ax = self.A@x
-		self.xTx = x.T@x
-		self.eig = x.T@self.Ax/self.xTx
-		self.grad = 2*((self.xTx)*self.Ax - (x.T@self.Ax)*x)/((self.xTx)*(self.xTx))
-		return(self.grad)
+    """
+    Finds the lowest eigenvalue of a symmetric matrix
+    by minimizing the Rayleigh quotient with gradient
+    descent
+    """
+    def __init__(self,A,eps=1e-4,maxiter=1000):
+        """
+        A - input matrix
+        eps - stops iterating when eigenvalue is not changing more than eps
+        maxiter - maximum number of iterations
+        """
+        self.A = A
+        self.maxiter = maxiter
+        self.eps = eps
+        self.x = np.random.rand(A.shape[0])
 
-	def optStep(self):
-		"""
-		Returns the optimal step length
-		"""
-		dR = self.grad
-		x = self.x
-		xTA = self.Ax.T
-		a = xTA@x
-		b = xTA@dR
-		c = dR.T@self.A@dR
-		e = self.xTx
-		f = x.T@dR
-		g =dR.T@dR
-		return( ( (a*g - e*c ) + np.sqrt( (e*c - a*g)**2 - 4*(a*f - e*b)*(b*g - c*f) ) )/(2*(b*g - c*f)))
+    def dR(self):
+        """
+        Returns the gradient of the Rayleigh quotient
+        """
+        x = self.x
+        self.Ax = self.A@x
+        self.xTx = x.T@x
+        self.eig = x.T@self.Ax/self.xTx
+        self.grad = 2*((self.xTx)*self.Ax - (x.T@self.Ax)*x)/((self.xTx)*(self.xTx))
+        return(self.grad)
 
-	def solve(self):
-		"""
-		Returns the lowest eigenvalue of A.
-		To be called after initialization.
-		"""
-		convergence = False
-		for i in range(self.maxiter):
-			grad = self.dR()
-			x = self.x/np.sqrt(self.xTx)
-			if np.mean(np.abs(self.Ax - self.eig*self.x)) <= self.eps:
-				convergence = True
-				break
-			self.x = self.x - self.optStep()*grad
+    def optStep(self):
+        """
+        Returns the optimal step length
+        """
+        dR = self.grad
+        x = self.x
+        xTA = self.Ax.T
+        a = xTA@x
+        b = xTA@dR
+        c = dR.T@self.A@dR
+        e = self.xTx
+        f = x.T@dR
+        g = dR.T@dR
+        return( ( (a*g - e*c ) + np.sqrt( (e*c - a*g)**2 - 4*(a*f - e*b)*(b*g - c*f) ) )/(2*(b*g - c*f)))
 
-		if not convergence:
-			print('WARNING: Did not converge. Try increasing maxiter or a smaller eps.')
+    def solve(self, timer=False):
+        """
+        Returns the lowest eigenvalue of A.
+        To be called after initialization.
+        """
+        if timer:
+            start = time.process_time()
+        convergence = False
+        val = 0
+        for i in range(self.maxiter):
+            grad = self.dR()
+            self.x = self.x - self.optStep()*grad
+            if np.sum(np.abs(grad)) < self.eps*(np.log(self.x.shape[0])):
+                convergence = True
+                break
 
-		return(self.eig,self.x)
+        if not convergence:
+            print('WARNING: Did not converge. Try increasing maxiter or a smaller eps.')    
+
+        if timer:
+            end = time.process_time()
+            t = end-start
+            return (self.eig,self.x,t)
+        else:
+            return(self.eig,self.x)
+
 
 
 if __name__ == '__main__':
